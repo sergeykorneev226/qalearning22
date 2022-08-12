23f452b5e4c489d00747111d677fe7f78ef2d35c
sergeykorneev226 <sergeykorneev226@gmail.com>
2022-08-12 11:56:22 +0300

    Add test_parametrize

---
+++ test_fixture71.py
@@ -0,0 +1,23 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+@pytest.mark.parametrize('language', ["ru", "en-gb"])
+class TestLogin(object):
+    def test_guest_should_see_login_link(self, browser, language):
+        link = f"http://selenium1py.pythonanywhere.com/{language}/"
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    # def test_guest_should_see_navbar_element(self, browser, language):
+
