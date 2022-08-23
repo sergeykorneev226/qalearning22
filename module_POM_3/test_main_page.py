10b34a8dad5fe220bdccd717aa32dbc31b0b53ac
sergeykorneev226 <sergeykorneev226@gmail.com>
2022-08-23 17:09:43 +0300

    Add locators.py

---
+++ module_POM_3/test_main_page.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from .pages.main_page import MainPage
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+
+
+class TestMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
